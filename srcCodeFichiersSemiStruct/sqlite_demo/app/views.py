65d4c9bf761753881dd479fbaa42a0c698bad5d5
Thibault Cavillon <thibault.cavillon@gmail.com>
2026-01-14 10:47:37 +0100

    jwt

--- srcCodeFichiersSemiStruct/sqlite_demo/app/views.py
+++ srcCodeFichiersSemiStruct/sqlite_demo/app/views.py
@@ -1,23 +1,40 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 import models
+import jwt
+import datetime
 
 app = Flask(__name__)
 models.init_db()
+
+SECRET_KEY = "mysecretkey"
+PASSWORD = "1234"  
 
 @app.route('/')
 def index():
     etudiants = models.get_etudiants()
     return render_template('index.html', etudiants=etudiants)
 
-@app.route('/new', methods=['GET', 'POST'])
+@app.route('/new')
 def new_etudiant():
-    if request.method == 'POST':
-        nom = request.form['nom']
-        addr = request.form['addr']
-        pin = request.form['pin']
-        models.ajouter_etudiant(nom, addr, pin)
-        return redirect(url_for('index'))
     return render_template('new.html')
+
+@app.route('/auth', methods=['POST'])
+def auth():
+    nom = request.form['nom']
+    addr = request.form['addr']
+    pin = request.form['pin']
+    password = request.form['password']
+    if password != PASSWORD:
+        return "Mot de passe incorrect", 403
+    token = jwt.encode({
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+    }, SECRET_KEY, algorithm="HS256")
+
+    models.ajouter_etudiant(nom, addr, pin)
+
+    resp = make_response(redirect(url_for('index')))
+    resp.set_cookie('token', token)
+    return resp
 
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_etudiant(id):
@@ -26,6 +43,7 @@ def edit_etudiant(id):
         pin = request.form['pin']
         models.update_etudiant(id, addr, pin)
         return redirect(url_for('index'))
+
     etudiants = models.get_etudiants()
     etudiant = next((e for e in etudiants if e[0] == id), None)
     return render_template('new.html', etudiant=etudiant)
